7a855494be0e470034ce764b11c0c58d36b5f31c
byron <byronmarineroo>
2025-05-26 09:59:12 -0600

    constructores e las clases futbolista y entrenador

--- Entrenador.java
+++ Entrenador.java
@@ -1,3 +1,43 @@
-public class Entrenador {
+public class Entrenador extends Persona {
+private String IdFederacion;
+
+public Entrenador() {
+}
+
+public Entrenador(String idFederacion) {
+    IdFederacion = idFederacion;
+}
+
+public Entrenador(int id, String nombre, String apellidos, int edad, String idFederacion) {
+    super(id, nombre, apellidos, edad);
+    IdFederacion = idFederacion;
+}
+
+public String getIdFederacion() {
+    return IdFederacion;
+}
+
+public void setIdFederacion(String idFederacion) {
+    IdFederacion = idFederacion;
+}
+
+@Override
+public void Concentrarse(){
+    System.out.println("Concentrandose como entrenador");
+}
+
+@Override
+public void Viajar(){
+    System.out.println("Viajando como entrenador");
+}
+
+public void dirigirPartido(){
+    System.out.println("Dirigiendo el partido");
+}
+
+public void DirigirEntrenamiento(){
+    System.out.println("Dirigiendo el entrenamiento");
+}
 
 }
+
--- Futbolista.java
+++ Futbolista.java
@@ -1,3 +1,56 @@
-public class Futbolista {
+public class Futbolista extends Persona{
+
+private int dorsal;
+private String demarcacion;
+
+public Futbolista() {
+}
+
+public Futbolista(int dorsal, String demarcacion) {
+    this.dorsal = dorsal;
+    this.demarcacion = demarcacion;
+}
+
+public Futbolista(int id, String nombre, String apellidos, int edad, int dorsal, String demarcacion) {
+    super(id, nombre, apellidos, edad);
+    this.dorsal = dorsal;
+    this.demarcacion = demarcacion;
+}
+
+public int getDorsal() {
+    return dorsal;
+}
+
+public void setDorsal(int dorsal) {
+    this.dorsal = dorsal;
+}
+
+public String getDemarcacion() {
+    return demarcacion;
+}
+
+public void setDemarcacion(String demarcacion) {
+    this.demarcacion = demarcacion;
+}
+
+@Override
+public void Concentrarse(){
+    System.out.println("Concentrandose como futbolista");
+}
+
+@Override
+public void Viajar(){
+    System.out.println("Viajando como futbolista");
+}
+
+public void jugarPartido(){
+    System.out.println("jugando partido");
+}
+
+public void entrenar(){
+    System.out.println("Entrenando");
+}
 
 }
+
+
